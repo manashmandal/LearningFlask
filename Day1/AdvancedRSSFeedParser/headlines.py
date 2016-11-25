0206122e202abd247e2c670c8b68234b8320ffdf
Manash Kumar Mandal <manashmndl@gmail.com>
2016-11-25 17:27:18 +0600

    added weatherapi

--- Day1/AdvancedRSSFeedParser/headlines.py
+++ Day1/AdvancedRSSFeedParser/headlines.py
@@ -2,6 +2,11 @@ from flask import Flask
 import feedparser
 from flask import render_template
 from flask import request
+from _credentials_ import keys
+
+import requests
+import json
+
 
 app = Flask(__name__)
 
@@ -23,9 +28,26 @@ def get_news():
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
+    weather = get_weather("London,UK")
 
-    return render_template("home.html", articles=feed['entries'])
+    return render_template("home.html", articles=feed['entries'], weather=weather)
 
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + keys.OPEN_WEATHER_MAP_API_KEY
+
+    try:
+        data = requests.get(api_url).json()
+        weather = {
+            "description": data['weather'][0]['description'],
+            "temperature": data['main']['temp'],
+            "city": data['name']
+        }
+
+    except json.decoder.JSONDecodeError:
+        weather = None
+
+    return weather
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
